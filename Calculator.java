97898a47daba7599962b82b677d085fdf358a270
mursalin117 <mursalinchowdhury117@gamil.com>
2019-04-10 00:04:12 +0600

    2nd Commit

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,12 @@
+import java.util.*;
+
 public class Calculator{
 
 	public static void main(String[] args) {
 		
+		Scanner in = new Scanner(System.in);
+		double a, b;
+		a = in.nextDouble();
+		b = in.nextDouble();
 	}
 }
